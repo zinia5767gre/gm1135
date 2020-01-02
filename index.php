841fe088dcb2c667071689ea9d4228de2fd45689
zinia5767gre <58773556+zinia5767gre@users.noreply.github.com>
2020-01-02 11:31:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://start-your-product.site/actmca/");
+header("Location: https://service.mcafee.com/webcenter/portal/cp;jsessionid=E1uNXpSArIoQsE0bPnFvwSi4jaUhB1fmO1ZdG7RMh1jrhKqcgbCX!-1078478202!1296122140?_adf.ctrl-state=r7zybpdly_4&_afrLoop=1203606957144644#!%40%40%3F_afrLoop%3D1203606957144644%26_adf.ctrl-state%3D4avfafppc_4");
 die();
 ?>
